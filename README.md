c243be6c20fe483ec68e1f16470237b4b205808f
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-10-29 13:38:47 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# newsletter-signup-app
+ This is a marketing mail app using mailchimp API
---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const https = require('https');
+
+const app = express();
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.get('/', (req,res)=>{
+    console.log('Server is running')
+    res.sendFile(__dirname + '/signup.html');
+})
+
+
+app.post('/', function (req, res){
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+   const data ={
+        members:[
+        {
+            email_address: email,
+            status:'subscibed',
+            merge_fields:{
+                FNAME:firstName,
+                LNAME:lastName
+            }
+        }
+        ]
+
+   }
+
+   const jsonData = JSON.stringify(data);
+
+   const url = 'https://us2.admin.mailchimp.com/lists/15a56fcaf8';
+
+   const options = {
+    method:'POST',
+    auth: 'ekip1:b352620ed9c08c7ea1a848b1f045f2f2-us2'
+   };
+
+  const request = https.request(url, options, function (response){
+        response.on('data', function (data){
+            console.log(JSON.parse(data));
+        });
+   });
+
+request.write(jsonData);
+request.end();
+
+});
+
+app.listen(3000, function(){
+    console.log('The server is running on port 3000.')
+});
+
+
+
+/* API Key
+b352620ed9c08c7ea1a848b1f045f2f2-us2 
+List Id
+15a56fcaf8
+*/
